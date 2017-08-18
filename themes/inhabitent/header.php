c519928f986f77513b685777b2e9d955adfa8bb9
Marie <marie@Lawrences-MacBook-Air.local>
2017-08-17 17:11:28 -0700

    added tent logo to the front page

--- themes/inhabitent/header.php
+++ themes/inhabitent/header.php
@@ -21,18 +21,21 @@
 
 			<header id="masthead" class="site-header" role="banner">
 				<div class="site-branding">
-					<div class="tent-logo">
-					<!-- <h1 class="site-title screen-reader-text"> -->
-						<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-						<h1 class= "site-title"><?php bloginfo( 'name' ); ?></h1>
-					</a>
+					<h1 class="site-title screen-reader-text">
+						<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></h1></a>
 					</div>
 					<p class="site-description"><?php bloginfo( 'description' ); ?></p>
 				</div><!-- .site-branding -->
 
 				<nav id="site-navigation" class="main-navigation" role="navigation">
+				<div class="header-background">
+			<div class="header">
+				<a href="<?php echo esc_url( home_url('/') ); ?>"><img class="white-logo-tent" src="<?php echo get_template_directory_uri(); ?>/images/logos/inhabitent-logo-tent-white.svg"></a>
 					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html( 'Primary Menu' ); ?></button>
 					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+					<?php get_search_form(); ?>
+				</div>
+				</div>
 				</nav><!-- #site-navigation -->
 			</header><!-- #masthead -->
 
