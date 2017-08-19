5eeb76b2b2bd7aa65c5f348b905d904f9d41233d
Marie <marie@Lawrences-MacBook-Air.local>
2017-08-19 13:12:02 -0700

    finished styling Find Us page

--- themes/inhabitent/header.php
+++ themes/inhabitent/header.php
@@ -30,7 +30,6 @@
 			
 
 				<nav id="site-navigation" class="main-navigation" role="navigation">
-						
 						<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html( 'Primary Menu' ); ?></button>
 						<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 						<span class="search-toggle"><i class="fa fa-search"></i></span>
