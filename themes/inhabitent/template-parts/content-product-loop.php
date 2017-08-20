01678ed0ddb4f739a0eb3d21c46168081fd99018
Marie <marie@Lawrences-Air.hitronhub.home>
2017-08-20 10:11:28 -0700

    made changes to content-product-loop template-part to have the link on the thumbnail

--- themes/inhabitent/template-parts/content-product-loop.php
+++ themes/inhabitent/template-parts/content-product-loop.php
@@ -10,10 +10,12 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 <header class="entry-header">
 	<?php if ( has_post_thumbnail() ) : ?>
-		<?php the_post_thumbnail( 'large' ); ?>
+		<a href="<?php the_permalink(); ?>">
+		<img src="<?php the_post_thumbnail( 'large' ); ?>
+		</a>
 	<?php endif; ?>
 
-	<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+	
 
 	<?php if ( 'post' === get_post_type() ) : ?>
 	<div class="entry-meta">
@@ -23,6 +25,7 @@
 </header><!-- .entry-header -->
 
 <div class="entry-content">
+<?php the_title( '<h2 class="entry-title">' , '</h2>' ); ?>
 	<?php echo CFS()->get( 'price' ); ?>
 	<?php //the_excerpt(); ?>
 </div><!-- .entry-content -->
