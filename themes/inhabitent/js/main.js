61b49804ac498756493d7281baa30cbaabce57ec
Marie <marie@Lawrences-Air.hitronhub.home>
2017-08-21 03:24:46 -0700

    reorganized stylesheets, troubleshooting

--- themes/inhabitent/js/main.js
+++ themes/inhabitent/js/main.js
@@ -15,11 +15,3 @@
 
 })( jQuery ); 
 
-//MENU 
-
-(function ( $ ) {
-    $(document).scroll(function () {
-      var $nav = $('.navbar-fixed-top');
-      $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-    });
-  });
